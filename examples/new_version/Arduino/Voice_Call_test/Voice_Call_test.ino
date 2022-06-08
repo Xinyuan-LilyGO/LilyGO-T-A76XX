f75520e6254708fb01bcf980806539c7a684488c
LilyGO <31160602+LilyGO@users.noreply.github.com>
2022-06-08 17:30:48 +0800

    test：VOICE CALL

---
+++ examples/new_version/Arduino/Voice_Call_test/Voice_Call_test.ino
@@ -0,0 +1,152 @@
+#define TINY_GSM_MODEM_SIM7600  //A7670 Compatible with SIM7600 AT instructions
+#define TINY_GSM_RX_BUFFER 1024 // Set RX buffer to 1Kb
+#define SerialAT Serial1
+// Define the serial console for debug prints, if needed
+#define TINY_GSM_DEBUG SerialMon
+
+// Set serial for debug console (to the Serial Monitor, default speed 115200)
+#define SerialMon Serial
+
+// See all AT commands, if wanted
+#define DUMP_AT_COMMANDS
+
+char mgsm[25];
+char number[] = "+380xxxxxxxxx";//Change the number you want to dial
+
+// set GSM PIN, if any
+#define GSM_PIN ""
+
+// Your GPRS credentials, if any
+const char apn[]  = "YOUR-APN";     //SET TO YOUR APN
+const char gprsUser[] = "";
+const char gprsPass[] = "";
+
+
+
+#include <TinyGsmClient.h>
+#include <SPI.h>
+#include <SD.h>
+#include <Ticker.h>
+
+#ifdef DUMP_AT_COMMANDS  // if enabled it requires the streamDebugger lib
+#include <StreamDebugger.h>
+StreamDebugger debugger(SerialAT, SerialMon);
+TinyGsm modem(debugger);
+#else
+TinyGsm modem(SerialAT);
+#endif
+
+
+#define UART_BAUD    115200
+#define PIN_DTR      25
+#define PIN_TX       26
+#define PIN_RX       27
+#define PWR_PIN      4
+#define BAT_ADC      35
+#define BAT_EN       12
+#define PIN_RI       33
+#define PIN_DTR      25
+#define RESET        5
+
+#define SD_MISO     2
+#define SD_MOSI     15
+#define SD_SCLK     14
+#define SD_CS       13
+
+
+void setup()
+{
+    // Set console baud rate
+    SerialMon.begin(UART_BAUD);
+    delay(10);
+
+    SerialMon.println("setup...");
+
+    pinMode(BAT_EN, OUTPUT);
+    digitalWrite(BAT_EN, HIGH);
+
+    //A7670 Reset
+    pinMode(RESET, OUTPUT);
+    digitalWrite(PWR_PIN, LOW);
+    delay(100);
+    digitalWrite(RESET, HIGH);
+    delay(1000);
+    digitalWrite(RESET, LOW);
+
+    pinMode(PWR_PIN, OUTPUT);
+    digitalWrite(PWR_PIN, LOW);
+    delay(100);
+    digitalWrite(PWR_PIN, HIGH);
+    delay(1000);
+    digitalWrite(PWR_PIN, LOW);
+
+    delay(1000);
+
+    SerialAT.begin(UART_BAUD, SERIAL_8N1, PIN_RX, PIN_TX);
+
+    // Restart takes quite some time
+    // To skip it, call init() instead of restart()
+    SerialMon.println("Initializing modem...");
+    if (!modem.init()) {
+        SerialMon.println("Failed to restart modem, attempting to continue without restarting");
+    }
+
+}
+
+void loop()
+{
+
+    SerialMon.println("Initializing modem...");
+    if (!modem.init()) {
+        SerialMon.println("Failed to restart modem, attempting to continue without restarting");
+    }
+
+    String name = modem.getModemName();
+    delay(500);
+    SerialMon.println("Modem Name: " + name);
+
+    String modemInfo = modem.getModemInfo();
+    delay(500);
+
+
+    DBG("Waiting for network...");
+    if (!modem.waitForNetwork()) {
+        delay(10000);
+        return;
+    }
+
+    if (modem.isNetworkConnected()) {
+        DBG("Network connected");
+    }
+
+    delay(3000);
+
+//*******************ring up ,Hang up after 60 s**************************
+    modem.sendAT("+CHUP");
+    // check for any of the three for simplicity
+    if (modem.waitResponse(1000L) != 1) {
+        DBG("Disconnect existing call");
+    }
+
+    sprintf(mgsm, "D%S;", number );
+
+    delay(200);
+    modem.sendAT(GF(mgsm));
+    // check for any of the three for simplicity
+    if (modem.waitResponse(10000L) != 1) {
+        DBG("call faill");
+    }
+
+    delay(60000);
+
+    modem.sendAT("+CHUP");
+    // check for any of the three for simplicity
+    if (modem.waitResponse(1000L) != 1) {
+        DBG("Disconnect existing call");
+    }
+
+    while (1) {
+        delay(1000);
+    }
+
+}
---
+++ examples/new_version/Arduino/Voice_Call_test/readme.md
@@ -0,0 +1,4 @@
+<h1 align = "center">T-A7670 Blynk_Console</h1>
+
+1. You need to change the number to call in the code 
+    char number[] = "+380xxxxxxxxx";  
