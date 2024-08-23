5a91003e5ea8c93190b342cc53017206e7820e2f
lewisxhe <lewishe@outlook.com>
2024-08-23 14:47:33 +0800

    Added example https://github.com/Xinyuan-LilyGO/LilyGO-T-A76XX/issues/118

---
+++ examples/SendLocationFromSMS/SendLocationFromSMS.ino
@@ -0,0 +1,226 @@
+/**
+ * @file      SendLocationFromSMS.ino
+ * @author    Lewis He (lewishe@outlook.com)
+ * @license   MIT
+ * @copyright Copyright (c) 2024  Shenzhen Xin Yuan Electronic Technology Co., Ltd
+ * @date      2024-08-23
+ * @note      SIM7670G can send SMS depending on the operator. This is not allowed in China. Please test it yourself.
+ */
+
+#include "utilities.h"
+
+
+// Define the serial console for debug prints, if needed
+#define TINY_GSM_DEBUG SerialMon
+
+// Set serial for debug console (to the Serial Monitor, default speed 115200)
+#define SerialMon Serial
+
+// See all AT commands, if wanted
+#define DUMP_AT_COMMANDS
+
+
+#include <TinyGsmClient.h>
+
+#ifdef DUMP_AT_COMMANDS  // if enabled it requires the streamDebugger lib
+#include <StreamDebugger.h>
+StreamDebugger debugger(SerialAT, SerialMon);
+TinyGsm modem(debugger);
+#else
+TinyGsm modem(SerialAT);
+#endif
+
+
+// It depends on the operator whether to set up an APN. If some operators do not set up an APN,
+// they will be rejected when registering for the network. You need to ask the local operator for the specific APN.
+// APNs from other operators are welcome to submit PRs for filling.
+// #define NETWORK_APN     "CHN-CT"             //CHN-CT: China Telecom
+
+
+#define SMS_TARGET  "+86xxxxxxx" //Change the number you want to send sms message
+
+
+
+float lat2      = 0;
+float lon2      = 0;
+float speed2    = 0;
+float alt2      = 0;
+int   vsat2     = 0;
+int   usat2     = 0;
+float accuracy2 = 0;
+int   year2     = 0;
+int   month2    = 0;
+int   day2      = 0;
+int   hour2     = 0;
+int   min2      = 0;
+int   sec2      = 0;
+uint8_t fixMode   = 0;
+
+
+bool loopGPS()
+{
+    Serial.println("Requesting current GPS/GNSS/GLONASS location");
+    if (modem.getGPS(&fixMode, &lat2, &lon2, &speed2, &alt2, &vsat2, &usat2, &accuracy2,
+                     &year2, &month2, &day2, &hour2, &min2, &sec2)) {
+
+        Serial.print("FixMode:"); Serial.println(fixMode);
+        Serial.print("Latitude:"); Serial.print(lat2, 6); Serial.print("\tLongitude:"); Serial.println(lon2, 6);
+        Serial.print("Speed:"); Serial.print(speed2); Serial.print("\tAltitude:"); Serial.println(alt2);
+        Serial.print("Visible Satellites:"); Serial.print(vsat2); Serial.print("\tUsed Satellites:"); Serial.println(usat2);
+        Serial.print("Accuracy:"); Serial.println(accuracy2);
+
+        Serial.print("Year:"); Serial.print(year2);
+        Serial.print("\tMonth:"); Serial.print(month2);
+        Serial.print("\tDay:"); Serial.println(day2);
+
+        Serial.print("Hour:"); Serial.print(hour2);
+        Serial.print("\tMinute:"); Serial.print(min2);
+        Serial.print("\tSecond:"); Serial.println(sec2);
+        return true;
+    }
+    return false;
+}
+
+
+void setup()
+{
+    Serial.begin(115200);
+    // Turn on DC boost to power on the modem
+#ifdef BOARD_POWERON_PIN
+    pinMode(BOARD_POWERON_PIN, OUTPUT);
+    digitalWrite(BOARD_POWERON_PIN, HIGH);
+#endif
+
+    // Set modem reset pin ,reset modem
+    pinMode(MODEM_RESET_PIN, OUTPUT);
+    digitalWrite(MODEM_RESET_PIN, !MODEM_RESET_LEVEL); delay(100);
+    digitalWrite(MODEM_RESET_PIN, MODEM_RESET_LEVEL); delay(2600);
+    digitalWrite(MODEM_RESET_PIN, !MODEM_RESET_LEVEL);
+
+    // Turn on modem
+    pinMode(BOARD_PWRKEY_PIN, OUTPUT);
+    digitalWrite(BOARD_PWRKEY_PIN, LOW);
+    delay(100);
+    digitalWrite(BOARD_PWRKEY_PIN, HIGH);
+    delay(1000);
+    digitalWrite(BOARD_PWRKEY_PIN, LOW);
+
+    // Set ring pin input
+    pinMode(MODEM_RING_PIN, INPUT_PULLUP);
+
+    // Set modem baud
+    SerialAT.begin(115200, SERIAL_8N1, MODEM_RX_PIN, MODEM_TX_PIN);
+
+    Serial.println("Start modem...");
+    delay(3000);
+
+    while (!modem.testAT()) {
+        delay(10);
+    }
+
+
+#ifdef NETWORK_APN
+    Serial.printf("Set network apn : %s\n", NETWORK_APN);
+    modem.sendAT(GF("+CGDCONT=1,\"IP\",\""), NETWORK_APN, "\"");
+    if (modem.waitResponse() != 1) {
+        Serial.println("Set network apn error !");
+    }
+#endif
+
+    Serial.println("Enabling GPS/GNSS/GLONASS");
+    while (!modem.enableGPS(MODEM_GPS_ENABLE_GPIO)) {
+        Serial.print(".");
+    }
+    Serial.println();
+    Serial.println("GPS Enabled");
+
+    // Set GPS Baud to 115200
+    modem.setGPSBaud(115200);
+
+
+    // Check network registration status and network signal status
+    int16_t sq ;
+    Serial.print("Wait for the modem to register with the network.");
+    RegStatus status = REG_NO_RESULT;
+    while (status == REG_NO_RESULT || status == REG_SEARCHING || status == REG_UNREGISTERED) {
+        status = modem.getRegistrationStatus();
+        switch (status) {
+        case REG_UNREGISTERED:
+        case REG_SEARCHING:
+            sq = modem.getSignalQuality();
+            Serial.printf("[%lu] Signal Quality:%d\n", millis() / 1000, sq);
+            delay(1000);
+            break;
+        case REG_DENIED:
+            Serial.println("Network registration was rejected, please check if the APN is correct");
+            return ;
+        case REG_OK_HOME:
+            Serial.println("Online registration successful");
+            break;
+        case REG_OK_ROAMING:
+            Serial.println("Network registration successful, currently in roaming mode");
+            break;
+        default:
+            Serial.printf("Registration Status:%d\n", status);
+            delay(1000);
+            break;
+        }
+    }
+    Serial.println();
+
+
+    Serial.printf("Registration Status:%d\n", status);
+    delay(1000);
+
+
+
+}
+
+
+void light_sleep(uint32_t ms)
+{
+    esp_sleep_enable_timer_wakeup(ms * 1000);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
+    esp_light_sleep_start();
+}
+
+void deep_sleep(uint32_t ms)
+{
+    esp_sleep_enable_timer_wakeup(ms * 1000);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
+    esp_deep_sleep_start();
+}
+
+
+void loop()
+{
+    if (loopGPS() == true) {
+
+        Serial.printf("The location was obtained successfully and sent to the %s number.\n", SMS_TARGET);
+
+        String msg_str = "Longitude:" + String(lon2, 6) + " ";
+        msg_str += "Latitude:" + String(lat2, 6) + "\n";
+        msg_str += "UTC Date:" + String(year2) + "/";
+        msg_str +=  String(month2) + "/";
+        msg_str +=  String(day2) + " \n";
+        msg_str += "UTC Time:" + String(hour2) + ":";
+        msg_str += String(min2) + ":";
+        msg_str += String(sec2);
+        msg_str += "\n";
+
+        Serial.print("MESSAGE:"); Serial.println(msg_str);
+
+        bool res = modem.sendSMS(SMS_TARGET, msg_str);
+        Serial.print("Send sms message ");
+        Serial.println(res ? "OK" : "fail");
+
+        // Wait print done
+        delay(3000);
+
+        // Deep sleep, wake up every 60 seconds for positioning
+        deep_sleep(60 * 1000); //60 Second
+    }
+
+    // Light sleep, reducing the allowed power consumption
+    light_sleep(5000);
+}
