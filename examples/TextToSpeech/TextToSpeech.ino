d74da4fbd9e70f63cf0d38d223f7eb1ef1abfd14
lewisxhe <lewishe@outlook.com>
2024-12-11 16:48:36 +0800

    Added TextToSpeech example

---
+++ examples/TextToSpeech/TextToSpeech.ino
@@ -0,0 +1,91 @@
+/**
+ * @file      TextToSpeech.ino
+ * @author    Lewis He (lewishe@outlook.com)
+ * @license   MIT
+ * @copyright Copyright (c) 2024  ShenZhen XinYuan Electronic Technology Co., Ltd
+ * @date      2024-12-11
+ * @note      Only support A7670X A7608X , Not support SIM7670G
+ */
+// See all AT commands, if wanted
+// #define DUMP_AT_COMMANDS
+
+#include "utilities.h"
+#include <TinyGsmClient.h>
+#include "Arduino.h"
+
+#ifdef DUMP_AT_COMMANDS  // if enabled it requires the streamDebugger lib
+#include <StreamDebugger.h>
+StreamDebugger debugger(SerialAT, Serial);
+TinyGsm modem(debugger);
+#else
+TinyGsm modem(SerialAT);
+#endif
+
+void setup()
+{
+    Serial.begin(115200); // Set console baud rate
+
+    Serial.println("Start Sketch");
+
+    SerialAT.begin(115200, SERIAL_8N1, MODEM_RX_PIN, MODEM_TX_PIN);
+
+#ifdef BOARD_POWERON_PIN
+    pinMode(BOARD_POWERON_PIN, OUTPUT);
+    digitalWrite(BOARD_POWERON_PIN, HIGH);
+#endif
+
+    // Set modem reset pin ,reset modem
+    pinMode(MODEM_RESET_PIN, OUTPUT);
+    digitalWrite(MODEM_RESET_PIN, !MODEM_RESET_LEVEL); delay(100);
+    digitalWrite(MODEM_RESET_PIN, MODEM_RESET_LEVEL); delay(2600);
+    digitalWrite(MODEM_RESET_PIN, !MODEM_RESET_LEVEL);
+
+    pinMode(BOARD_PWRKEY_PIN, OUTPUT);
+    digitalWrite(BOARD_PWRKEY_PIN, LOW);
+    delay(100);
+    digitalWrite(BOARD_PWRKEY_PIN, HIGH);
+    delay(100);
+    digitalWrite(BOARD_PWRKEY_PIN, LOW);
+
+    // Check if the modem is online
+    Serial.println("Start modem...");
+
+    int retry = 0;
+    while (!modem.testAT(1000)) {
+        Serial.println(".");
+        if (retry++ > 10) {
+            digitalWrite(BOARD_PWRKEY_PIN, LOW);
+            delay(100);
+            digitalWrite(BOARD_PWRKEY_PIN, HIGH);
+            delay(1000);
+            digitalWrite(BOARD_PWRKEY_PIN, LOW);
+            retry = 0;
+        }
+    }
+    Serial.println();
+
+    delay(5000);
+
+    // Mode 1. Start to synth and play,<text> is in UCS2 coding format.
+    String text = "6B228FCE4F7F75288BED97F3540862107CFB7EDF";
+    if (modem.textToSpeech(text, 1)) {
+        Serial.println("Play successfully.");
+    }
+
+}
+
+uint32_t count = 0;
+
+void loop()
+{
+    String text = "millis" + String(count++);
+    // Mode: 2. Start to synth and play,<text> is in ASCII coding format,
+    if (modem.textToSpeech(text, 2)) {
+        Serial.println("Play successfully.");
+    }
+    delay(1000);
+}
+
+#ifndef TINY_GSM_FORK_LIBRARY
+#error "No correct definition detected, Please copy all the [lib directories](https://github.com/Xinyuan-LilyGO/LilyGO-T-A76XX/tree/main/lib) to the arduino libraries directory , See README"
+#endif
