b4e406ffc80d644a01550d28dbf9977d88d125ac
lewisxhe <lewishe@outlook.com>
2025-07-13 17:12:39 +0800

    Added camera storage to sd card example[no ci]

---
+++ examples/CameraCaptureToSDCard/CameraCaptureToSDCard.ino
@@ -0,0 +1,239 @@
+/**
+ * @file      CameraCaptureToSDCard.ino
+ * @author    Lewis He (lewishe@outlook.com)
+ * @license   MIT
+ * @copyright Copyright (c) 2025  ShenZhen XinYuan Electronic Technology Co., Ltd
+ * @date      2025-07-13
+ * @note      Sketch is only suitable for LilyGo-A7670X-S3 version,Other versions are not supported
+ *
+ */
+#include <esp_camera.h>
+#include <Wire.h>
+#include <FS.h>
+#include <SD.h>
+
+#ifndef LILYGO_A7670X_S3
+#error "Sketch is only suitable for LilyGo-A7670X-S3 version , Other versions are not supported"
+#endif
+
+#define ENABLE_BATTERY_MON
+
+#include <Wire.h>
+#include "utilities.h"
+
+#define uS_TO_S_FACTOR          1000000ULL  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP           180          /* Time ESP32 will go to sleep (in seconds) */
+#define BATTERY_VOLTAGE_LOW     3000        // Set low voltage to sleep mode
+
+RTC_DATA_ATTR int bootCount = 0;
+
+bool setCameraPower(bool enable)
+{
+    static bool started = false;
+
+    if (!started) {
+        Wire.begin(BOARD_SDA_PIN, BOARD_SCL_PIN);
+        Wire.beginTransmission(0x28);
+        if (Wire.endTransmission() != 0) {
+            Serial.println("Camera power chip not found!");
+            return false;
+        }
+    }
+    started = true;
+    uint8_t vdd[] = {
+        0x04,   /*reg address*/
+        0x7C,   /*REG03 DVDD1 1.496V*/
+        0x7C,   /*REG04 DVDD2 1.496v*/
+        0xCA,   /*REG05 AVDD1 3.0V*/
+        0xB1    /*REG06 AVDD2 2.8V*/
+    };
+    Wire.beginTransmission(0x28);
+    Wire.write(vdd, sizeof(vdd) / sizeof(vdd[0]));
+    Wire.endTransmission();
+
+    uint8_t control[] = {0x0E, 0x0F};
+    if (!enable) {
+        control[1] = 0x00;
+    }
+    Wire.beginTransmission(0x28);
+    Wire.write(control, sizeof(control) / sizeof(control[0]));
+    Wire.endTransmission();
+    return true;
+}
+
+uint16_t get_battery_voltage()
+{
+    uint16_t vol = analogReadMilliVolts(BOARD_BAT_ADC_PIN) * 2;
+    Serial.printf("Voltage:%u\n", vol);
+    return vol;
+}
+
+void set_device_to_sleep()
+{
+    Serial.println("Enter esp32 goto deepsleep!");
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+    delay(200);
+    esp_deep_sleep_start();
+    Serial.println("This will never be printed");
+    while (1);
+}
+
+
+bool setupSD()
+{
+    SPI.begin(BOARD_SCK_PIN, BOARD_MISO_PIN, BOARD_MOSI_PIN);
+
+    if (!SD.begin(BOARD_SD_CS_PIN)) {
+        Serial.println("Card Mount Failed");
+        return false;
+    }
+    uint8_t cardType = SD.cardType();
+
+    if (cardType == CARD_NONE) {
+        Serial.println("No SD card attached");
+        return false;
+    }
+
+    Serial.print("SD Card Type: ");
+    if (cardType == CARD_MMC) {
+        Serial.println("MMC");
+    } else if (cardType == CARD_SD) {
+        Serial.println("SDSC");
+    } else if (cardType == CARD_SDHC) {
+        Serial.println("SDHC");
+    } else {
+        Serial.println("UNKNOWN");
+    }
+
+    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+    Serial.printf("SD Card Size: %lluMB\n", cardSize);
+    return true;
+}
+
+void setup()
+{
+    Serial.begin(115200);
+
+    Serial.println();
+
+    //Increment boot number and print it every reboot
+    ++bootCount;
+    Serial.println("Boot number: " + String(bootCount));
+
+
+#ifdef ENABLE_BATTERY_MON
+    int tryCount = 4;
+    while (tryCount--) {
+        if (get_battery_voltage() < (BATTERY_VOLTAGE_LOW + 100)) {
+            set_device_to_sleep();
+        } else {
+            Serial.println("Battery voltage is normal");
+        }
+        delay(500);
+    }
+#endif
+
+    // Turn on the camera power
+    if (!setCameraPower(true)) {
+        Serial.println("Failed to initialize Camera power chip!"); return;
+    }
+
+    // Initialize sd card
+    if (!setupSD()) {
+        Serial.println("Failed to initialize SD card! Please check SD card!"); return;
+    }
+
+    // Enable / disable power save mode (1 disabled, 0 enabled)
+    pinMode(BOARD_POWER_SAVE_MODE_PIN, OUTPUT);
+    digitalWrite(BOARD_POWER_SAVE_MODE_PIN, HIGH);
+
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = CAMERA_Y2_PIN;
+    config.pin_d1 = CAMERA_Y3_PIN;
+    config.pin_d2 = CAMERA_Y4_PIN;
+    config.pin_d3 = CAMERA_Y5_PIN;
+    config.pin_d4 = CAMERA_Y6_PIN;
+    config.pin_d5 = CAMERA_Y7_PIN;
+    config.pin_d6 = CAMERA_Y8_PIN;
+    config.pin_d7 = CAMERA_Y9_PIN;
+    config.pin_xclk = CAMERA_XCLK_PIN;
+    config.pin_pclk = CAMERA_PCLK_PIN;
+    config.pin_vsync = CAMERA_VSYNC_PIN;
+    config.pin_href = CAMERA_HREF_PIN;
+    config.pin_sccb_sda = CAMERA_SIOD_PIN;
+    config.pin_sccb_scl = CAMERA_SIOC_PIN;
+    config.pin_pwdn = CAMERA_PWDN_PIN;
+    config.pin_reset = CAMERA_RESET_PIN;
+    config.xclk_freq_hz = 20000000;
+    config.frame_size = FRAMESIZE_HD;       //1280x720
+    config.pixel_format = PIXFORMAT_JPEG;  // JPEG formart
+    config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+    config.fb_location = CAMERA_FB_IN_PSRAM;
+    config.jpeg_quality = 5;
+    config.fb_count = 2;
+
+    // camera init
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK) {
+        Serial.printf("Camera init failed with error 0x%x\n", err);
+        return;
+    }
+
+    sensor_t *s = esp_camera_sensor_get();
+    // initial sensors are flipped vertically and colors are a bit saturated
+    if (s->id.PID == OV3660_PID) {
+        s->set_vflip(s, 1);        // flip it back
+        s->set_brightness(s, 1);   // up the brightness just a bit
+        s->set_saturation(s, -2);  // lower the saturation
+    }
+
+    // s->set_vflip(s, 1);
+    // s->set_hmirror(s, 1);
+
+    // Capture camera photo
+    camera_fb_t *frame = esp_camera_fb_get();
+
+    if (frame) {
+
+        // Stored in the camera directory
+        if (!SD.exists("/camera")) {
+            if (SD.mkdir("/camera")) {
+                Serial.println("Created camera directory!");
+            }
+        }
+
+        String filename = "/camera/";
+        filename.concat(bootCount);
+        filename.concat(".jpg");
+
+        uint32_t startTime = millis();
+        File jpg = SD.open(filename, "w");
+        if (jpg) {
+            Serial.printf("JPG created successfully,filename:%s write image data,framesize:%u * %u", filename.c_str(), frame->width, frame->height);
+            jpg.write(frame->buf, frame->len);
+            Serial.printf("JPG was written successfully, taking %lu ms\n", millis() - startTime);
+        } else {
+            Serial.printf("JPG created failed!");
+        }
+        jpg.close();
+        esp_camera_fb_return(frame);
+    } else {
+        Serial.println("Capturing camera failed!");
+    }
+}
+
+void loop()
+{
+    Serial.println("Disbale camera");
+    esp_camera_deinit();
+
+    Serial.println("Power off camera");
+    setCameraPower(false);
+
+    Serial.println("Enter esp32 goto deepsleep!");
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+    delay(200);
+    esp_deep_sleep_start();
+}
