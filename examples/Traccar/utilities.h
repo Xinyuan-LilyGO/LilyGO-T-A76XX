aa3543111c0ca456351779d93681219574ef3af6
lewisxhe <lewishe@outlook.com>
2025-07-14 14:33:00 +0800

    Update pinsout[no ci]

--- examples/Traccar/utilities.h
+++ examples/Traccar/utilities.h
@@ -287,8 +287,8 @@
     #define SIMSHIELD_RADIO_CS                  (39)
     #define SIMSHIELD_RADIO_IRQ                 (9)
     #define SIMSHIELD_RADIO_RST                 (40)
-    #define SIMSHIELD_RS_RX                     (42)
-    #define SIMSHIELD_RS_TX                     (41)
+    #define SIMSHIELD_RS_RX                     (41)
+    #define SIMSHIELD_RS_TX                     (42)
     #define SIMSHIELD_SDA                       (2)
     #define SIMSHIELD_SCL                       (1)
     #define SerialRS485                         Serial2
@@ -486,6 +486,68 @@
 
     // This board has connected the ADC circuit to the modem, and the battery voltage can be read via AT+CBC
     #define MODEM_CONNECTED_ADC_PIN
+
+
+#elif defined(LILYGO_SIM7080G_S3)
+
+    #define SerialAT                    Serial1
+    #define MODEM_BAUDRATE              (115200)
+
+    #define PWDN_GPIO_NUM               (-1)
+    #define RESET_GPIO_NUM              (18)
+    #define XCLK_GPIO_NUM               (8)
+    #define SIOD_GPIO_NUM               (2)
+    #define SIOC_GPIO_NUM               (1)
+    #define VSYNC_GPIO_NUM              (16)
+    #define HREF_GPIO_NUM               (17)
+    #define PCLK_GPIO_NUM               (12)
+    #define Y9_GPIO_NUM                 (9)
+    #define Y8_GPIO_NUM                 (10)
+    #define Y7_GPIO_NUM                 (11)
+    #define Y6_GPIO_NUM                 (13)
+    #define Y5_GPIO_NUM                 (21)
+    #define Y4_GPIO_NUM                 (48)
+    #define Y3_GPIO_NUM                 (47)
+    #define Y2_GPIO_NUM                 (14)
+
+    #define I2C_SDA                     (15)
+    #define I2C_SCL                     (7)
+
+    #define PMU_INPUT_PIN               (6)
+
+    #define BUTTON_CONUT                (1)
+    #define USER_BUTTON_PIN             (0)
+    #define BUTTON_ARRAY                {USER_BUTTON_PIN}
+
+    #define BOARD_PWRKEY_PIN            (41)
+    #define MODEM_DTR_PIN               (42)
+    #define MODEM_RING_PIN              (3)
+    #define MODEM_RX_PIN                (4)
+    #define MODEM_TX_PIN                (5)
+
+    #define SDMMC_CMD                   (39)
+    #define SDMMC_CLK                   (38)
+    #define SDMMC_DATA                  (40)
+
+    #ifndef TINY_GSM_MODEM_SIM7080G
+        #define TINY_GSM_MODEM_SIM7080G
+    #endif
+
+    //! The following pins are for SimShield and need to be used with SimShield
+    //! 以下引脚针对SimShield,需要搭配SimShield 
+    #define SIMSHIELD_MOSI                      (11)
+    #define SIMSHIELD_MISO                      (13)
+    #define SIMSHIELD_SCK                       (12)
+    #define SIMSHIELD_SD_CS                     (21)
+    #define SIMSHIELD_RADIO_BUSY                (48)
+    #define SIMSHIELD_RADIO_CS                  (45)
+    #define SIMSHIELD_RADIO_IRQ                 (8)
+    #define SIMSHIELD_RADIO_RST                 (47)
+    #define SIMSHIELD_RS_RX                     (2)
+    #define SIMSHIELD_RS_TX                     (1)
+    #define SIMSHIELD_SDA                       (44)
+    #define SIMSHIELD_SCL                       (43)
+    #define SerialRS485                         Serial2
 
 #elif defined(LILYGO_T_RELAY_S3_SIMSHIELD)
 
